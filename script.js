c54c2c3ab1fbb36eb525fce109b0328807a123e7
Abudagir <kondaktor3@gmail.com>
2023-06-27 22:30:22 +0200

    init

---
+++ script.js
@@ -0,0 +1,84 @@
+//1 Exercise
+console.log("1 Exercise");
+
+const value1 = true;
+const value2 = 1000;
+const value3 = null;
+const value4 = undefined;
+
+const str1 = String(value1);
+const str2 = String(value2);
+const str3 = String(value3);
+const str4 = String(value4);
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+console.log(typeof str4);
+
+//2 Exercise
+console.log("2 Exercise");
+
+const value5 = "123a";
+const value6 = "1000";
+const value7 = null;
+const value8 = undefined;
+const value9 = false;
+
+const num5 = Number(value5);
+const num6 = Number(value6);
+const num7 = Number(value7);
+const num8 = Number(value8);
+const num9 = Number(value9);
+
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+// console.log(num8);
+// console.log(num9);
+
+console.log(typeof num5);
+console.log(typeof num6);
+console.log(typeof num7);
+console.log(typeof num8);
+console.log(typeof num9);
+
+//3 Exercise
+console.log("3 Exercise");
+
+const value10 = null;
+const value11 = undefined;
+const value12 = "";
+const value13 = 0;
+const value14 = " ";
+const value15 = 1000;
+const value16 = "1200";
+
+const bool10 = Boolean(value10);
+const bool11 = Boolean(value11);
+const bool12 = Boolean(value12);
+const bool13 = Boolean(value13);
+const bool14 = Boolean(value14);
+const bool15 = Boolean(value15);
+const bool16 = Boolean(value16);
+
+// console.log(bool10);
+// console.log(bool11);
+// console.log(bool12);
+// console.log(bool13);
+// console.log(bool14);
+// console.log(bool15);
+// console.log(bool16);
+
+console.log(typeof bool10);
+console.log(typeof bool11);
+console.log(typeof bool12);
+console.log(typeof bool13);
+console.log(typeof bool14);
+console.log(typeof bool15);
+console.log(typeof bool16);
